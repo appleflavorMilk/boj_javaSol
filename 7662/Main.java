3273c61db743eb53d7ab4b3405a1ed5e6fc57543
박장우 <wkddn020423@gmail.com>
2024-09-10 07:55:19 +0900

    C:/Program Files/Git/7662

---
+++ 7662/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        //deque 선언
+        Deque<Integer> deque = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        //Test case 입력
+        int Testcase = scanner.nextInt();
+        int[] arr=new int[n];
+
+    }
+
+}
